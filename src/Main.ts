3703de50ce09c308a94cbc6e7ce63edcd311340f
wxnnvs <136503688+wxnnvs@users.noreply.github.com>
2024-03-21 15:58:29 +0100

    test

--- src/Main.ts
+++ src/Main.ts
@@ -1,3 +1,19 @@
-ModAPI.displayToChat({msg: "hello, this is eaglerforge!"})
-ModAPI.logger.loginfo({string: "you can log a string to console"})
-ModAPI.uwuify({string: "and even uwuify a string?"})
+ModAPI.addEventListener("sendchatmessage", function(event){
+    let spam: NodeJS.Timeout;
+
+    if (event.message.includes("/deeznuts")) {
+
+        event.preventDefault = true;
+
+        setTimeout(function() {
+            spam = setInterval(function(){
+                ModAPI.player.sendChatMessage({msg: event.message.split("/deeznuts")[1].trim() + Math.floor(Math.random() * 1000)});
+            }, 1000);
+        }, 1000);
+        ModAPI.addEventListener("keypress", function(event){
+            if (event.key == 2) {
+                clearInterval(spam);
+            }
+        });
+    }
+});
